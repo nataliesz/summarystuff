213a6f9d975e3fee7c7f8cf4e079f6e22185b189
YOUR NAME <118699689+YzFu04@users.noreply.github.com>
2024-04-15 09:10:26 -0400

    add Roxygen description, debug

--- R/categorical_yz.R
+++ R/categorical_yz.R
@@ -1,26 +1,34 @@
+#' @title
+#' generate explanatory data analysis for numeric variable
+#' @description
+#' Given a numeric variable, this function generate EDA including summary statistics and visualization.
 
-categorical_ss <- function(df, var) {
+categorical_ss <- function(var) {
   # categorical
   var_name <- as.character(var)
-  n_missing <- sum(is.na(df$var))
-  rate_missing <- sum(!is.na(df$var)) / length(df$var)
-  n_unique <- length(unique(df$var))
-  value_unique <- unique(df$var)
+  n_missing <- sum(is.na(var))
+  rate_missing <- sum(!is.na(var)) / length(var)
+  n_unique <- length(unique(var))
+  value_unique <- unique(var)
 
-  result <- data.frame(var_name, n_missing, rate_missing, n_unique, value_unique)
+  result <- list(var_name, n_missing, rate_missing, n_unique, value_unique)
   return(result)
 }
 
 categorical_v <- function(df, var) {
   # barplot
-  plot <- ggplot(df, aes(x = var)) +
-    geom_bar()
+  plot <- ggplot2::ggplot(df, aes(x = var)) + geom_bar()
   return(plot)
 }
 
 categorical_y <- function(df, var, y) {
   # boxplot with y variable
-  plot <- ggplot(df, aes(x = var, y = y)) +
-    geom_boxplot()
+  plot <- ggplot2::ggplot(df, aes(x = var, y = y)) + geom_boxplot()
   return(plot)
 }
+
+# devtools::document()
+# devtools::install()
+# open a new session
+# library(xkcd)
+# xkcd(600)
