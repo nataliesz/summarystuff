4cc6d23f8b4a82bfc72ce0fe60172f0bcfed74ea
Natalie Szewczyk <nszewczyk@smith.edu>
2024-04-17 15:13:12 -0400

    deleted old function

--- R/numeric.R
+++ R/numeric.R
@@ -2,7 +2,7 @@
 #'
 #' @description Generates exploratory data analysis for numeric variables
 #'
-#' @importFrom plot plot::ggplot2
+#' @importFrom dplyr summarise
 #'
 #' @param df A dataframe provided by the user
 #' @param var A variable to compute statistics for
@@ -12,20 +12,6 @@
 #'
 #' Given a numeric variable, this function generate EDA including distribution and visualization.
 #' @import geom_histogram ggplot2::geom_histogram
-
-num_summary_stat <- function(df, var) {
-  # categorical
-  var_name <- as.character(var)
-  n_missing <- sum(is.na(df$var))
-  rate_missing <- sum(!is.na(df$var)) / length(df$var)
-  mean <- mean(df$var)
-  median <- median(df$var)
-  sd <- sd(df$var)
-  min <- min(df$var)
-  max <- max(df$var)
-
-  result <- list(var_name, n_missing, rate_missing, mean, median, sd, min, max)
-}
 
 numeric_stats <- function(df, var) {
   df |>
@@ -40,11 +26,13 @@ numeric_stats <- function(df, var) {
                      rate_mising = sum(!is.na(var)) / length(var))
 }
 
-num_distribution <- function(df, var) {
+histogram <- function(df, var) {
+  hist()
+}
+
+num_distribution <- function(df) {
   # histogram
-  plot <- ggplot2::ggplot(df, aes(x = var)) +
-    geom_histogram()
-  return(plot)
+  hist(df$var)
 }
 
 numeric_y <- function(df, var, y) {
