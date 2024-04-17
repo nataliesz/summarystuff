b08b763361edf3e42a9105c9077361030bc88a34
Natalie Szewczyk <nszewczyk@smith.edu>
2024-04-17 15:45:24 -0400

    attempting to fix the numeric_stats function to work across all columns

--- R/nataliework.R
+++ R/nataliework.R
@@ -11,6 +11,27 @@ numeric_stats <- function(df, var) {
                      rate_mising = sum(!is.na(var)) / length(var))
   }
 
+numeric_stats <- function(df) {
+  for (i in col(df)){
+    dplyr::summarise(min = min(df[[i]]),
+                     q1 = quantile(df[[i]], 0.25),
+                     median = median(df[[i]]),
+                     mean = mean(df[[i]]),
+                     q3 = quantile(df[[i]], 0.75),
+                     max = max(df[[i]]),
+                     sd = sd(df[[i]]),
+                     n_missing = sum(is.na(df[[i]])),
+                     rate_mising = sum(!is.na(df[[i]])) / length(df[[i]]))
+  }
+}
+
+num_distribution <- function(df, var) {
+  # histogram
+  plot <- ggplot2::ggplot(df, aes(x = var)) +
+    geom_histogram()
+  return(plot)
+}
+
 #identify outliers
 outliers <- function(dataset, x) {
   out <- summary(dataset$x)
