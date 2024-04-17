3d177e5762c0b1d7d2822e01cb30587c304c7fe5
rashydan <radaniel@smith.edu>
2024-04-17 12:03:14 -0400

    updated roxygenote for categorical, example not included

--- R/categorical.R
+++ R/categorical.R
@@ -1,8 +1,11 @@
-#' @title
-#' generate explanatory data analysis for numeric variable
+#' @title Generate explanatory data analysis for categorical variable
+#'
 #' @description
-#' Given a numeric variable, this function generate EDA including summary statistics and visualization.
-
+#' Given a categorical variable, this function generate EDA including summary statistics and visualization.
+#'
+#' @importFrom ggplot2 ggplot
+#'
+#'
 cat_summary_stat <- function(var) {
   # categorical
   n <- table(var)
@@ -21,17 +24,43 @@ cat_summary_stat <- function(var) {
   return(result)
 }
 
+#' @param var A categorical variable for[`summarystuff`].
+#' @return A data frame containing summary statistics such as count of missing
+#'   values, rate of missing values, number of unique categories, and unique
+#'   category values.
+#' @examples
+
+
 cat_distribution <- function(df, var) {
   # barplot
   plot <- ggplot2::ggplot(df, aes(x = var)) + geom_bar()
   return(plot)
 }
+#' Plot Categorical Variable Distribution
+#' @param df
+#' @param var
+#' @return A ggplot object displaying the distribution of the categorical variable.
+#'
+#' @examples
+#'
+
+
 
 cat_ <- function(df, var, y) {
   # boxplot with y variable
   plot <- ggplot2::ggplot(df, aes(x = var, y = y)) + geom_boxplot()
   return(plot)
 }
+#' Plot Categorical Variable Against Another Variable.
+#' @param df
+#' @param var
+#' @param y The numerical variable to be plotted on y-axis.
+#'
+#' @return A ggplot object displaying the box plot.
+#'
+#' @examples
+#'
+
 
 # devtools::document()
 # devtools::install()
