ce239603117361fb2ec8b9c66304f92a57c97341
YOUR NAME <118699689+YzFu04@users.noreply.github.com>
2024-04-17 15:37:37 -0400

    update Roxygen

--- R/categorical.R
+++ R/categorical.R
@@ -7,12 +7,12 @@
 #'
 #' @param df A dataframe provided by the user
 #' @param var A categorical variable to compute statistics for
-#' @param y A variable to create visualization with
-#' @param ... currently unused
+#'
+#' @return A data frame containing summary statistics such as count of missing
+#'   values, rate of missing values, number of unique categories, and unique
+#'   category values.
 #'
 #' @examples
-#'
-#' Given a categorical variable, this function generate EDA including distribution and visualization.
 
 cat_summary_stat <- function(df, var) {
 # categorical
@@ -27,33 +27,20 @@ cat_summary_stat <- function(df, var) {
   return(results)
 }
 
-#' @param var A categorical variable for[`summarystuff`].
-#' @return A data frame containing summary statistics such as count of missing
-#'   values, rate of missing values, number of unique categories, and unique
-#'   category values.
+#' Plot Categorical Variable Distribution
+#' @param df
+#' @param var
+#'
+#' @return A ggplot object displaying the distribution of the categorical variable.
+#'
 #' @examples
-
 
 cat_distribution <- function(df, var) {
   # barplot
   plot <- ggplot2::ggplot(df, aes(x = {{var}})) + geom_bar()
   return(plot)
 }
-#' Plot Categorical Variable Distribution
-#' @param df
-#' @param var
-#' @return A ggplot object displaying the distribution of the categorical variable.
-#'
-#' @examples
-#'
 
-
-
-cat_relation <- function(df, var, y) {
-  # boxplot with y variable
-  plot <- ggplot2::ggplot(df, aes(x = {{var}}, y = {{y}})) + geom_boxplot()
-  return(plot)
-}
 #' Plot Categorical Variable Against Another Variable.
 #' @param df
 #' @param var
@@ -62,8 +49,12 @@ cat_relation <- function(df, var, y) {
 #' @return A ggplot object displaying the box plot.
 #'
 #' @examples
-#'
 
+cat_relation <- function(df, var, y) {
+  # boxplot with y variable
+  plot <- ggplot2::ggplot(df, aes(x = {{var}}, y = {{y}})) + geom_boxplot()
+  return(plot)
+}
 
 # devtools::document()
 # devtools::install()
