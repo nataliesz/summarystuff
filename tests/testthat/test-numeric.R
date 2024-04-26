31dcf5d248e679f5fa335423f68ef1663a53029d
Natalie Szewczyk <nszewczyk@smith.edu>
2024-04-26 08:25:30 -0400

    altered vignettes and attempted to fix tests

--- tests/testthat/test-numeric.R
+++ tests/testthat/test-numeric.R
@@ -1,4 +1,3 @@
 test_that("dataframe is valid", {
-  expect_true(is.list(summary_stats(mtcars)))
-  expect_equal(class(summary_stats(mtcars, mtcars$mpg)), "data.frame")
+  expect_true(is.list(summary_stats.numeric()))
 })
