020ba55d865a8ae26f91d2e629261320892e0e45
Natalie Szewczyk <nszewczyk@smith.edu>
2024-04-26 09:05:47 -0400

    test for distribution function (need to figure out how to get it to work for factor?)

--- tests/testthat/test-numeric.R
+++ tests/testthat/test-numeric.R
@@ -5,3 +5,10 @@ test_that("dataframe is valid", {
 test_that("summary_stats is valid", {
   expect_true(is.list(summary_stats.factor(mtcars$gear, mtcars)))
 })
+
+test_that("distributions work", {
+  vdiffr::expect_doppelganger(
+    title = "mtcars mpg",
+    fig = distribution.numeric(mtcars$mpg, mtcars)
+  )
+})
